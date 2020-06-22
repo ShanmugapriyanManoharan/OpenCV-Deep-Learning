2eb78459710941d71db5ae4aca87684bf819e9fc
ShanmugapriyanManoharan <46359211+ShanmugapriyanManoharan@users.noreply.github.com>
2020-06-23 00:31:23 +0200

    Update Lesson1.m

--- 10_Matlab Practice/Lesson1.m
+++ 10_Matlab Practice/Lesson1.m
@@ -4,10 +4,10 @@
 1. We borrowed $1000 at a 10% annual interest rate. If we did not make a payment for two years, and assuming there is no penalty for non-payment, how much do we owe now?
 Assign the result to a variable called debt.
 
-```
+'''
 p = 1000; r = 0.1; t = 2;
 debt = p*((1+r)^t)
-```
+'''
 
 ```
 debt =
